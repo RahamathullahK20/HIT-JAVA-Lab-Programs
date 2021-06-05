0b742a86fc956bdacdcfa2fdff9a6172a2cd30e3
Rahamathullah K <42862351+RahamathullahK20@users.noreply.github.com>
2021-06-05 23:14:19 +0530

    Add files via upload

---
+++ AddTwoNumbers.java
@@ -0,0 +1,9 @@
+package lab.programs;
+public class AddTwoNumbers {
+
+	public static void main(String[] args) {
+		int num1 = 50, num2 = 200, sum; 
+		sum = num1 + num2;
+		System.out.println("Sum of two numbers:" + sum);
+	}
+}
