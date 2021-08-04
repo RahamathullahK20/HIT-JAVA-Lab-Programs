16335588286ce910f5b542db81bc7d220cecdc9a
Rahamathullah K <42862351+RahamathullahK20@users.noreply.github.com>
2021-08-05 00:16:08 +0530

    Update SendEmail.java

--- Project/BulkEmail/yt/SendEmail.java
+++ Project/BulkEmail/yt/SendEmail.java
@@ -1,6 +1,7 @@
 package Mail.hit.day41;
 
 import java.util.Properties;
+import java.util.StringTokenizer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -23,8 +24,9 @@ public class SendEmail {
 		properties.put("mail.smtp.host","smtp.gmail.com");
 		properties.put("mail.smtp.port","587");
 		
-		String myAccountEmail = "xxxxxxxxx@gmail.com";
-		String password = "xxxxxxxxx";
+		
+		String myAccountEmail = "xxxxxxxxxxx@gmail.com";
+		String password = "xxxxxxxxxx";
 		
 		Session session = Session.getInstance(properties, new Authenticator() {
 		@Override
@@ -45,8 +47,19 @@ public class SendEmail {
 		Message message = new MimeMessage(session);
 		message.setFrom(new InternetAddress(myAccountEmail) );
 		message.setRecipient(Message.RecipientType.TO, new InternetAddress(recepient));
+		    StringTokenizer st = new StringTokenizer(recepient, "@");
+		    String recepientname = st.nextToken();
+		    System.out.println("Fetching Username..........."+"\nSent to "+recepientname+"");
+		  
 		message.setSubject("My First Email Program....");
-		message.setText("Hello There....");
+		message.setText("Dear "+ recepientname +".\n"
+		+"\t Greetings to you. I hope you are at the best of your health."
+		+"\n Welcome to My Github Account - https://github.com/Aslam-web/EmailApplication"
+		+"\n\n\n\n Thanks & Regards"+"\n Mr.M.N Aslam,"
+		+"\n Java Developer Trainer,"
+		+"\n Haaris Infotech Institutions,"
+		+"\n Email : aslam1qqqq@gmail.com,"
+		+"\n Phone: +91 63799 71782." );
 		return message;
 		
 	}catch (Exception ex) {
